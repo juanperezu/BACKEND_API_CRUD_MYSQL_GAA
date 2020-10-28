fb64ac9f7ed3da7f12c28ba393e126158baf1efa
juanperezu <juanperezu@gmail.com>
2020-10-28 13:52:26 -0500

    primer envio routes crud

--- routes/routes.js
+++ routes/routes.js
@@ -33,30 +33,42 @@ res.json({message:'Estudiante Matriculado'});
 })// fin nuevo-estudiante
 
 // editar un Estudiante
+/*
+        "id":1,
+        "documento": "1000444579",
+        "tipo_documento": "TI",
+        "nombres": "Leysi ",
+        "correo":"asasasasasa",
+        "apellidos": "vlaentina Modificado",
+        "contrasena": "34c958e8afa723e3806b37fffa2d64d2ee0ceef9",
+        "telefono_celular": "3023458971",
+        "fecha_nacimiento": "1999-10-09",
+        "institucion_id": 5*/
 router.put('/estudiante/:id',(req,res)=>{
     const { nombres, apellidos, correo, documento, telefono_celular,
         fecha_nacimiento, institucion_id } = req.body;
     const { id } = req.body;
-    mysqlConnection.query(`UPDATE actores SET nombres=?,apellidos=?,correo=?,
-                           documento=?,telefono_celular=?,
-                          fecha_nacimiento=?,institucion_id=?  WHERE id=?`,
-            [nombres, apellidos, correo, documento, telefono_celular,
+    mysqlConnection.query(`UPDATE actores SET nombres=?,apellidos=?,correo=?, documento=?,telefono_celular=?,
+                          fecha_nacimiento=?,institucion_id=?  WHERE id=? `,[nombres, apellidos, correo, documento, telefono_celular,
             fecha_nacimiento, institucion_id, id], (err, rows, fields) => {
-                if (!error) {
+                if (!err) {
                     res.json({ status: 'Estudiante actualizado' });
                 } else {
                     console.log(err);
                 }// fin si
 
-
-            }//  
-    )
-
-
-
-
+           }//  
+    );
 })// fin editar
 
-
+router.get('/estudiante/:id',(req,res)=>{
+    const {id} =req.params;
+    mysqlConnection.query('SELECT * from actores where id=?',[id],(err,rows,fields)=>{
+    if(!err){
+       res.json(rows[0]); 
+    }else{
+    console.log(err);
+    }});
+    })// fin
 
 module.exports=router;
